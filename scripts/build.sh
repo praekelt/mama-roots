bc4765b09426998b35594b512df0bd89a87310ca
Simon de Haan <simon@praekeltfoundation.org>
2015-09-16 10:16:22 +0200

    install the local package

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,7 @@ REPO_DIR="$WORKSPACE/$REPO"
 set -e
 
 cd $REPO_DIR
-npm install lodash
-npm install axis
-npm install jeet
-npm install rupture
 npm install roots
+npm install .
 make build
 cp -r ./public/* ${BUILDDIR}/${REPO}
