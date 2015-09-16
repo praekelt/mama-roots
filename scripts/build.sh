a45a1aa1c0ff8bb65f115472bf759d3229738dcd
Simon de Haan <simon@praekeltfoundation.org>
2015-09-16 10:13:00 +0200

    change order

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@ REPO_DIR="$WORKSPACE/$REPO"
 
 cd $REPO_DIR
 npm install lodash
-npm install roots
 npm install axis
 npm install jeet
+npm install roots
 make build
 cp -r ./public/* ${BUILDDIR}/${REPO}
